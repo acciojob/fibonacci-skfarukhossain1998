36298bc3effc8e5c89ef386355298f7c781a998d
skfarukhossain1998 <127541802+skfarukhossain1998@users.noreply.github.com>
2023-04-23 00:49:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function fibonacci(num) {
-// your code here
+  if (num === 0) {
+    return 0;
+  } else if (num === 1 || num === 2) {
+    return 1;
+  } else if (num < 0 || num > 50) {
+    return "Invalid input";
+  } else {
+    let prev1 = 1;
+    let prev2 = 1;
+    let curr = 0;
+    for (let i = 3; i <= num; i++) {
+      curr = prev1 + prev2;
+      prev2 = prev1;
+      prev1 = curr;
+    }
+    return curr;
+  }
 }
 
 module.exports = fibonacci;
